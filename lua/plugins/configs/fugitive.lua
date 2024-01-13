df44db98e78ed3974290d21a6b0af59cff88f72f
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2024-01-13 19:09:26 +0100

    feat: add keymap for git diff

--- lua/plugins/configs/fugitive.lua
+++ lua/plugins/configs/fugitive.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>vs", vim.cmd.Git, { desc='[V]ersion control [S]tatus' });
 vim.keymap.set("n", "<leader>vc", function() vim.cmd.Git('commit') end, { desc='[V]ersion control [C]ommit' });
+vim.keymap.set("n", "<leader>vd", vim.cmd.Gvdiffsplit, { desc='[V]ersion control [D]iff' });
