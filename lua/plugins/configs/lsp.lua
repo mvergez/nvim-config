5f1f7d30c9407562131719335b5b6832b523ff01
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2024-01-13 19:10:02 +0100

    feat: whichkey and improve doc
    For whichkey to read it

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -5,13 +5,15 @@ lsp_zero.on_attach(function(client, bufnr)
   -- to learn the available actions
   --lsp_zero.default_keymaps({buffer = bufnr})
   local opts = {buffer = bufnr, remap = false}
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set('n', 'gr', function() require('telescope.builtin').lsp_references() end, opts)
-  vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, {desc="[G]o to [D]efinition", buffer = bufnr, remap = false})
+  vim.keymap.set('n', 'gr', function() require('telescope.builtin').lsp_references() end, {desc="[G]o to [R]eference", buffer = bufnr, remap = false})
+  vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, {desc="[G]o to [H]over", buffer = bufnr, remap = false})
+  vim.keymap.set("n", '<leader>ca', vim.lsp.buf.code_action, {desc="[C]ode [A]ction", buffer = bufnr, remap = false})
+  vim.keymap.set('n', '<leader>ce', vim.diagnostic.open_float, {desc="[C]ode [D]etails"})
 end)
 
-require('lspconfig').pyright.setup{}
 
+require('lspconfig').pyright.setup{}
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = { "lua_ls", "pyright", "clangd" },
@@ -19,3 +21,17 @@ require('mason-lspconfig').setup({
     lsp_zero.default_setup,
   },
 })
+require('lspconfig').clangd.setup {
+    cmd = { "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--cross-file-rename",
+    "--header-insertion=iwyu",
+    "-j=12",
+    "--query-driver=/usr/bin/**/clang-*,/bin/clang,/bin/clang++,/usr/bin/gcc,/usr/bin/g++",
+    "--all-scopes-completion",
+    "--completion-style=detailed",
+    "--header-insertion-decorators",
+    "--pch-storage=memory"
+}
+}
